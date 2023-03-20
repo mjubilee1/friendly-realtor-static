c19b42fed3308c268ce0ac5f7f64aa79ffc3ef95
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-03-20 17:24:22 -0400

    update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -104,22 +104,12 @@ export const socialMedia = [
   {
     id: "social-media-1",
     icon: instagram,
-    link: "https://www.instagram.com/",
+    link: "https://www.instagram.com/friendlyrealtor.app/",
   },
   {
     id: "social-media-2",
     icon: facebook,
-    link: "https://www.facebook.com/",
-  },
-  {
-    id: "social-media-3",
-    icon: twitter,
-    link: "https://www.twitter.com/",
-  },
-  {
-    id: "social-media-4",
-    icon: linkedin,
-    link: "https://www.linkedin.com/",
+    link: "https://www.facebook.com/profile.php?id=100091290482188",
   },
 ];
 
