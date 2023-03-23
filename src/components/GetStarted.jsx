1aabf966efaa91b769550a953e5ed44f79a410f5
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-03-22 21:34:49 -0400

    handle adding image

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,7 +3,7 @@ import styles from '../style'
 import {arrowUp} from '../assets'
 
 const GetStarted = () => (
-    <a href="https://testflight.apple.com/join/88VYYxs4" className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <a href="https://testflight.apple.com/join/88VYYxs4" className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-primary-gradient p-[2px] cursor-pointer`}>
 		<div className={`${styles.flexCenter} flex-col bg-secondary w-[100%] h-[100%] rounded-full`}>
 			<div className={`${styles.flexStart} flex-row`}>
 				<p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
