1e28f23b798dba8bb31cab849a50bf8b068d61f1
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-03-30 14:46:27 -0400

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from "./style";
 import { Billing, Business, CardDeal, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 import { FirebaseProvider } from './context';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProfilePage from './pages/profile';
 
 const NotFound404 = lazy(() => import('./pages/404'));
 
@@ -10,25 +11,27 @@ const App = () => {
 	return <FirebaseProvider>
 		<BrowserRouter>
 			<div className={`bg-secondary w-full overflow-hidden`}>
-				{window.location.pathname === "/" &&
-				<div>
 					<div className={`${styles.paddingX} ${styles.flexCenter}`}>
 						<div className={`${styles.boxWidth}`}>
 							<Navbar />
 						</div>
 					</div>
 
+					{window.location.pathname === "/" &&
 					<div className={`bg-secondary ${styles.flexStart}`}>
 						<div className={`${styles.boxWidth}`}>
 							<Hero />
 						</div>
-					</div>
-				</div>}
+					</div>}
 				<div className={`bg-secondary ${styles.paddingX} ${styles.flexCenter}`}>
 				<Routes>
 					<Route
 						path="*"
 						element={<NotFound404 />}
+					/>
+					<Route
+						path="profile/*"
+						element={<ProfilePage />}
 					/>
 					<Route
 						path="/"
---
+++ src/pages/profile/index.jsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+
+export const ProfilePage = memo(() => {
+
+  return (
+    <div className="text-white text-5xl text-center py-40 h-screen">
+      <h1>HEllo</h1>
+    </div>
+  );
+});
+
+export default ProfilePage;
