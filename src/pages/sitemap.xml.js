d86ba41ff24895f9c3605a36a798b5a1b5c21d3b
Montrell <mjubilee@leaptodigital.com>
2023-04-22 18:08:13 -0400

    handle dynamic sitemap

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,21 +1,17 @@
-//pages/sitemap.xml.js
-const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
+import { collection, getDocs } from 'firebase/firestore';
+import { firestore } from '../context';
 
-function generateSiteMap(posts) {
+function generateSiteMap(data) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
-     </url>
-     ${posts
-       .map(({ id }) => {
+	 		<url>
+				<loc>https://friendlyrealtor.app</loc>
+			</url>
+     ${data.filter((data) => data.userName)
+       .map(({ userName }) => {
          return `
        <url>
-           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+           <loc>${`https://friendlyrealtor.app/profile/${userName}`}</loc>
        </url>
      `;
        })
@@ -24,17 +20,17 @@ function generateSiteMap(posts) {
  `;
 }
 
-function SiteMap() {
-  // getServerSideProps will do the heavy lifting
-}
+function SiteMap() {}
 
 export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
+	const userRef = collection(firestore, "users");
+	const querySnapshot = await getDocs(userRef);
+	let profiles = [];
+	querySnapshot.forEach((doc) => {
+		profiles.push(doc.data())
+	});
 
-  // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts);
+  const sitemap = generateSiteMap(profiles);
 
   res.setHeader('Content-Type', 'text/xml');
   // we send the XML to the browser
