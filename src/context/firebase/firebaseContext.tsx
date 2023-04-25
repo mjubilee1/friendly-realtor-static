8200c40aa8b74e8d33727d62a7f146bbf12927d1
Montrell <mjubilee@leaptodigital.com>
2023-04-24 21:21:41 -0400

    update firebase id

--- src/context/firebase/firebaseContext.tsx
+++ src/context/firebase/firebaseContext.tsx
@@ -25,7 +25,7 @@ export function FirebaseProvider({ children }) {
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-      measurementId: process.env.NEXT_PUBLIC__FIREBASE_MEASUREMENT_ID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
     });
     setFirebaseApp(app);
   }, []);
