ec1559dc491edf72466234eafde92beca94c0ceb
Montrell <mjubilee@leaptodigital.com>
2023-04-24 21:31:30 -0400

    handle updating facebook pixel

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,13 @@ const NavBar = () => {
                 index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
               } text-white`}
             >
-              {el.to ? <a href={`${el.to}`} target='_blank'>{el.title}</a> : <a href={`${el.id}`}>{el.title}</a>}
+              {el.to ? (
+                <a href={`${el.to}`} target="_blank">
+                  {el.title}
+                </a>
+              ) : (
+                <a href={`${el.id}`}>{el.title}</a>
+              )}
             </li>
           );
         })}
@@ -45,7 +51,13 @@ const NavBar = () => {
                     index === navLinks.length - 1 ? 'mr-0' : 'mb-4'
                   } text-white`}
                 >
-              {el.to ? <a href={`${el.to}`} target='_blank'>{el.title}</a> : <a href={`${el.id}`}>{el.title}</a>}
+                  {el.to ? (
+                    <a href={`${el.to}`} target="_blank">
+                      {el.title}
+                    </a>
+                  ) : (
+                    <a href={`${el.id}`}>{el.title}</a>
+                  )}
                 </li>
               );
             })}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import SEO from '../../next-seo.config';
 import { DefaultSeo } from 'next-seo';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar, Footer } from '../components';
 import styles from '../styles/styles';
 import { Ubuntu } from 'next/font/google';
 import Head from 'next/head';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import * as fbq from '../util';
 
 const ubuntu = Ubuntu({
   subsets: ['latin'],
@@ -14,12 +17,46 @@ const ubuntu = Ubuntu({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview();
+
+    const handleRouteChange = () => {
+      fbq.pageview();
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <div>
       <Head>
         <title>Friendly Realtor</title>
         <link rel="icon" href="/logo.png" />
       </Head>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+					!function(f,b,e,v,n,t,s)
+					{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+					n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+					if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+					n.queue=[];t=b.createElement(e);t.async=!0;
+					t.src=v;s=b.getElementsByTagName(e)[0];
+					s.parentNode.insertBefore(t,s)}(window, document,'script',
+					'https://connect.facebook.net/en_US/fbevents.js');
+					fbq('init', ${fbq.FB_PIXEL_ID});
+					fbq('track', 'PageView');
+          `,
+        }}
+      />
       <div
         className={`flex min-h-screen flex-col items-center justify-between p-12 ${ubuntu.className}`}
       >
