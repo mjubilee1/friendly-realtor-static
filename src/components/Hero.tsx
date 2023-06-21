d66ec41a495b7b70ad907fe4125cbe501d28d5c6
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-06-21 16:41:44 -0400

    update realtor page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,55 +31,47 @@ const Hero = () => {
   };
 
   return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 flex justify-start items-start flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 mb-8 font-ubuntu font-semibold ss:text-[32px] text-[32px] text-white">
+    <section id="home" className={`flex flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 flex justify-center items-center md:px-12 px-6`}>
+        <div className="text-center md:text-left">
+          <h1 className="mb-8 font-ubuntu font-semibold text-[32px] text-white">
             Designed specifically for realtors, this comprehensive realtor toolkit is a valuable
             resource that offers a wide range of resources and information to assist you in making
             informed decisions throughout your clients' home buying journey.
           </h1>
+          <h6 className="font-ubuntu text-sm leading-relaxed">
+            Our mission at Friendly Realtor is to achieve three vital economic objectives - increased
+            revenue, expanded market share, and improved profitability - within 24 months through the
+            provision of innovative real estate agent solutions. As a company, we are committed to
+            helping new real estate agents save time and grow their businesses, which is why we aim to
+            achieve these objectives. We recognize the challenges that new real estate agents face,
+            and our goal is to simplify the real estate process, provide valuable resources, and
+            foster long-term relationships with our clients. We believe that our products and services
+            will enable new real estate agents to achieve their full potential in the industry.
+            Through our innovative solutions, we are dedicated to empowering new real estate agents,
+            providing them with the tools and resources necessary to build successful businesses. We
+            are committed to creating a supportive and collaborative environment that encourages
+            growth and development. Ultimately, our mission is to make a positive impact on the real
+            estate industry by helping new real estate agents succeed, fostering long-term
+            partnerships, and delivering exceptional value to our clients.
+          </h6>
+          <div className="flex flex-col md:flex-row items-center mt-8">
+            <AddLink onClick={handleClick}>
+              <Image
+                src={apple}
+                alt="friendly_realtor_app"
+                className="w-44 h-44 object-contain mb-4 md:mb-0 cursor-pointer"
+              />
+            </AddLink>
+            <Button
+              color=""
+              className="px-4 py-4 bg-primary rounded-md text-base hover:bg-black md:text-lg"
+              onClick={handleNavigateStipe}
+            >
+              Advertise With Us
+            </Button>
+          </div>
         </div>
-        <h6 className="font-ubuntu">
-          Our mission at Friendly Realtor is to achieve three vital economic objectives - increased
-          revenue, expanded market share, and improved profitability - within 24 months through the
-          provision of innovative real estate agent solutions. As a company, we are committed to
-          helping new real estate agents save time and grow their businesses, which is why we aim to
-          achieve these objectives. We recognize the challenges that new real estate agents face,
-          and our goal is to simplify the real estate process, provide valuable resources, and
-          foster long-term relationships with our clients. We believe that our products and services
-          will enable new real estate agents to achieve their full potential in the industry.
-          Through our innovative solutions, we are dedicated to empowering new real estate agents,
-          providing them with the tools and resources necessary to build successful businesses. We
-          are committed to creating a supportive and collaborative environment that encourages
-          growth and development. Ultimately, our mission is to make a positive impact on the real
-          estate industry by helping new real estate agents succeed, fostering long-term
-          partnerships, and delivering exceptional value to our clients.
-        </h6>
-        <div className="flex items-center mt-8">
-          <AddLink onClick={handleClick}>
-            <Image
-              src={apple}
-              alt="friendly_realtor_app"
-              className="w-44 object-contain mr-5 cursor-pointer"
-            />
-          </AddLink>
-          <Button
-            type="submit"
-            color=""
-            className="px-4 py-4 bg-primary rounded-md text-xl"
-            onClick={handleNavigateStipe}
-          >
-            Advertise With Us.
-          </Button>
-        </div>
-      </div>
-
-      <div className={`flex-1 flex ${styles.flexCenter} md:mr-0  my-10 relative`}>
-        <Image src={iphoneScreen} className="w-[100%] h-[100%] relative z-[5]" alt="" srcSet="" />
-        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
-        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
       </div>
     </section>
   );
