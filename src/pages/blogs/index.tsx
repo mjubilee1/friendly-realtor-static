afce067d548521c4bc44b064225a7baeebae7bcf
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-06-21 16:20:31 -0400

    make blogs responsive

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -23,7 +23,7 @@ const AllBlogPage = () => {
   return (
     <Container>
       <Head>
-        <title> Discover Friendly Realtors Blog Post</title>
+        <title>Discover Friendly Realtors Blog Post</title>
         <meta
           name="description"
           content="Explore our latest blog post to discover valuable insights and tips from friendly realtors."
@@ -34,21 +34,25 @@ const AllBlogPage = () => {
           content="Explore our latest blog post to discover valuable insights and tips from friendly realtors."
         />
       </Head>
-      <Header as="h1" className="pb-8">
+      <Header as="h1" className="pb-8 text-center">
         Discover Friendly Realtors Blog Post
       </Header>
-      <div className="grid grid-cols-3 gap-24">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {blogPosts.map((post) => (
           <Link href={`/blogs/${post.fields.slug}`} key={post.sys.id} className="text-center">
-            <Header as="h4" className="font-semibold mb-2">
-              {post.fields.title}
-            </Header>
-            <Image
-              src={post.fields.featureImage.fields.file.url}
-              alt={post.fields.featureImage.fields.title}
-              size="w-full h-64"
-            />
-            <p className="mt-2">Author: {post.fields.author.fields.name}</p>
+            <div className="max-w-xs mx-auto h-full bg-gray-500 rounded-lg shadow-md overflow-hidden">
+              <Image
+                src={post.fields.featureImage.fields.file.url}
+                alt={post.fields.featureImage.fields.title}
+                size="w-full h-64"
+              />
+              <div className="p-4">
+                <Header as="h4" className="font-semibold mb-2">
+                  {post.fields.title}
+                </Header>
+                <p className="mt-2">Author: {post.fields.author.fields.name}</p>
+              </div>
+            </div>
           </Link>
         ))}
       </div>
