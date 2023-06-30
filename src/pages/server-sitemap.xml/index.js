88627a30b7406b6ac7fd2f8970f40bf3eb266e83
Montrell <jubileeinvestmentsdmv@gmail.com>
2023-06-30 01:13:43 -0400

    try this

--- src/pages/server-sitemap.xml/index.js
+++ src/pages/server-sitemap.xml/index.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../../context';
 import { getServerSideSitemap } from 'next-sitemap';
 import { fetchEntries } from '../../utils/contentfulUtil';
@@ -6,9 +6,12 @@ import { fetchEntries } from '../../utils/contentfulUtil';
 export const getServerSideProps = async (ctx) => {
   const userRef = collection(firestore, 'users');
   const querySnapshot = await getDocs(userRef);
-  let profiles = [];
+  let profilesSitemaps = [];
   querySnapshot.forEach((doc) => {
-    profiles.push(doc.data());
+    profilesSitemaps.push({
+      loc: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/users/${doc.data().username || doc.data().userName}`,
+      lastmod: new Date(doc.data().updatedAt).toISOString(),
+    });
   });
 
   const blogPosts = await fetchEntries('blogPost');
@@ -18,7 +21,20 @@ export const getServerSideProps = async (ctx) => {
     lastmod: new Date(blogPost.sys.updatedAt).toISOString(),
   }));
 
-  const fields = [...blogPostSitemaps];
+  // Add properties document to sitemap
+  const propertiesDocRef = doc(firestore, 'properties', 'properties');
+  const propertiesDocSnapshot = await getDoc(propertiesDocRef);
+	const properties = propertiesDocSnapshot.data()
+	
+	let propertiesSitemaps = []
+	properties.ids?.map((prop) => {
+		propertiesSitemaps.push({
+			loc: `${process.env.NEXT_PUBLIC_DOMAIN_URL}/listings?id=${prop}`,
+			lastmod: new Date(properties.updatedAt).toISOString(),
+		})
+	})
+
+  const fields = [...blogPostSitemaps, ...profilesSitemaps, ...propertiesSitemaps];
   return getServerSideSitemap(fields, ctx);
 };
 
