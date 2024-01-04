4c931ff96dba9dbc8fc8d84defd6a2765320855e
Montrell <jubileeinvestmentsdmv@gmail.com>
2024-01-03 21:36:14 -0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,25 +26,30 @@ const Hero = () => {
   };
 
   return (
-    <div className="flex flex-row flex-wrap items-start gap-4">
-      <div className="flex-1 flex flex-col h-full">
-        <div className="h-full">
-          <div className="text-6xl font-light mb-6">Empowering Realtors Together</div>
-          <div>
-            Embark on a seamless realtor empowerment journey with our innovative platform.
-            Experience the benefits of our free event hosting feature, effortlessly connecting with
-            attendees. Simplifying event coordination and prioritizing local connections, the Event
-            Center catalyzes trust, fostering a community where networking transforms real estate
-            success.
-          </div>
+    <div className="flex flex-col md:flex-row items-start gap-4">
+      <div className="flex-1 flex flex-col md:h-full">
+        <div className="md:mb-6 text-4xl md:text-6xl font-light mb-6">
+          Empowering Realtors Together
         </div>
-        <div className="mt-4">
+        <div className="mb-4">
+          Embark on a seamless realtor empowerment journey with our innovative platform. Experience
+          the benefits of our free event hosting feature, effortlessly connecting with attendees.
+          Simplifying event coordination and prioritizing local connections, the Event Center
+          catalyzes trust, fostering a community where networking transforms real estate success.
+        </div>
+        <div>
           <Button color="secondary" onClick={handleClick} className="py-4">
             Download Mobile App
           </Button>
         </div>
       </div>
-      <Image src={friendlyrealtor} alt="friendly_realtor_image" className="w-[40rem] h-[40rem]" />
+      <div className="mt-4 md:mt-0">
+        <Image
+          src={friendlyrealtor}
+          alt="friendly_realtor_image"
+          className="w-full md:w-[40rem] h-auto"
+        />
+      </div>
     </div>
   );
 };
